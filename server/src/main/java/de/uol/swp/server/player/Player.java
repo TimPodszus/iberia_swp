98914b1bfdad3a7df94ff64cadf27aff88343b7c
Felix Siemer <Felix.Siemer@student.ibs-ol.de>
2024-08-28 22:14:07 +0200

    setStartingPosition() Methode wurde zum Spieler hinzugefügt.

--- server/src/main/java/de/uol/swp/server/player/Player.java
+++ server/src/main/java/de/uol/swp/server/player/Player.java
@@ -1,6 +1,7 @@
 package de.uol.swp.server.player;
 
 import de.uol.swp.server.cards.Card;
+import de.uol.swp.server.cards.CityCard;
 import de.uol.swp.server.city.City;
 import de.uol.swp.server.role.Role;
 import de.uol.swp.server.user.User;
@@ -12,16 +13,41 @@ import lombok.Setter;
 import java.util.List;
 
 @Getter
-@AllArgsConstructor
 public class Player {
     private final String username;
     private final Role role;
     @Setter
-    private City city;
+    private City currentPosition;
     @Setter
     private List<Card> cards;
     private final User user;
 
+    public Player(String username, Role role, List<Card> cards, User user)
+    {
+        this.username = username;
+        this.role = role;
+        this.cards = cards;
+        this.user = user;
+    }
+
+    public void setStartingPosition(City city) throws Exception {
+        boolean validRequest = false;
+        int cityCardCount = 0;
+        for(Card card : cards){
+            if (card instanceof CityCard cityCard) {
+                cityCardCount++;
+                if(cityCard.getCity() == city){
+                    validRequest = true;
+                }
+            }
+        }
+        if(validRequest || cityCardCount == 0){
+            setCurrentPosition(city);
+        }else{
+            throw new Exception("Keine valide Stadt ausgewählt! Du musst eine Stadt die du auf der Hand hast " +
+                    "auswählen!");
+        }
+    }
     public void playCard(Card card) {
         //not implemented
     }
