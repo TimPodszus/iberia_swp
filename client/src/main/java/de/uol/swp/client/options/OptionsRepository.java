3305e958ddb5a27c9fa54c38c834f145f8822183
larshemmers <lars.hemmers@gmail.com>
2024-09-11 14:53:42 +0200

    moved option values into properties file

--- client/src/main/java/de/uol/swp/client/options/OptionsRepository.java
+++ client/src/main/java/de/uol/swp/client/options/OptionsRepository.java
@@ -3,14 +3,66 @@ package de.uol.swp.client.options;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.Properties;
+
 @Getter
 @Setter
 public class OptionsRepository {
+    private static final String OPTIONS_FILE = "options.properties";
+    private static final String VOLUME_IDENTIFIER = "VOLUME";
+    private static final String CHAT_ENABLED_IDENTIFIER = "CHAT_ENABLED";
+
+    private static final double DEFAULT_VOLUME = 100.0;
+    private static final boolean DEFAULT_CHAT_ENABLED = true;
+
+    private Properties properties;
     private boolean chatEnabled;
     private double volume;
 
+    /**
+     * Constructs an OptionsRepository object and initializes its properties.
+     * <p>
+     * This constructor attempts to load properties from the options file.
+     * If an IOException occurs, it sets default values for chatEnabled and volume.
+     */
     public OptionsRepository() {
-        this.chatEnabled = true;
-        this.volume = 100.0;
+        try {
+            // Load properties from the options file
+            loadProperties();
+
+            // Parse and set the chatEnabled property
+            this.chatEnabled = Boolean.parseBoolean(properties.getProperty(CHAT_ENABLED_IDENTIFIER));
+
+            // Parse and set the volume property
+            this.volume = Double.parseDouble(properties.getProperty(VOLUME_IDENTIFIER));
+        } catch (IOException e) {
+            // Set default values if an IOException occurs
+            this.chatEnabled = DEFAULT_CHAT_ENABLED;
+            this.volume = DEFAULT_VOLUME;
+        }
+    }
+
+    /**
+     * Loads properties from the options file.
+     *
+     * @throws IOException if an I/O error occurs when reading from the input stream.
+     */
+    private void loadProperties() throws IOException {
+        // Initialize the properties object
+        properties = new Properties();
+
+        // Get the path to the resource directory
+        String resourcePath = Objects.requireNonNull(Thread.currentThread().getContextClassLoader().getResource("")).getPath();
+
+        // Construct the full path to the options file
+        String path = resourcePath + OPTIONS_FILE;
+
+        // Load the properties from the file input stream
+        try (FileInputStream fis = new FileInputStream(path)) {
+            properties.load(fis);
+        }
     }
 }
