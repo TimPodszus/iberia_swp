1de7a9bd51241dc087634d8f61462c4d450db4c5
marten.wempe <marten.wempe@vierol.de>
2024-09-12 16:10:20 +0200

    implement WelcomeScreen

--- client/src/main/java/de/uol/swp/client/main/MainMenuPresenter.java
+++ client/src/main/java/de/uol/swp/client/main/MainMenuPresenter.java
@@ -1,5 +1,9 @@
 package de.uol.swp.client.main;
 
+import de.uol.swp.client.lobby.event.ShowCurrentGamesViewEvent;
+import de.uol.swp.client.main.event.ShowLobbyOverviewViewEvent;
+import de.uol.swp.client.options.event.ShowOptionViewEvent;
+import de.uol.swp.common.exception.UnsopportedMethodExeption;
 import org.greenrobot.eventbus.Subscribe;
 
 import com.google.inject.Inject;
@@ -20,6 +24,10 @@ import javafx.scene.control.ListView;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 /**
@@ -28,13 +36,16 @@ import java.util.List;
  * @author Marco Grawunder
  * @see de.uol.swp.client.AbstractPresenter
  * @since 2019-08-29
- *
  */
 public class MainMenuPresenter extends AbstractPresenter {
 
     public static final String FXML = "/fxml/MainMenuView.fxml";
 
     private static final Logger LOG = LogManager.getLogger(MainMenuPresenter.class);
+
+    private static final ShowLobbyOverviewViewEvent showLobbyOverviewViewMessage = new ShowLobbyOverviewViewEvent();
+    private static final ShowCurrentGamesViewEvent showCurrentGamesViewMessage = new ShowCurrentGamesViewEvent();
+    private static final ShowOptionViewEvent showOptionViewMessage = new ShowOptionViewEvent();
 
     private ObservableList<String> users;
 
@@ -48,7 +59,7 @@ public class MainMenuPresenter extends AbstractPresenter {
 
     /**
      * Handles successful login
-     *
+     * <p>
      * If a LoginSuccessfulResponse is posted to the EventBus the loggedInUser
      * of this client is set to the one in the message received and the full
      * list of users currently logged in is requested.
@@ -65,7 +76,7 @@ public class MainMenuPresenter extends AbstractPresenter {
 
     /**
      * Handles new logged in users
-     *
+     * <p>
      * If a new UserLoggedInMessage object is posted to the EventBus the name of the newly
      * logged in user is appended to the user list in the main menu.
      * Furthermore if the LOG-Level is set to DEBUG the message "New user {@literal
@@ -80,14 +91,16 @@ public class MainMenuPresenter extends AbstractPresenter {
 
         LOG.debug("New user {}  logged in,", message.getUsername());
         Platform.runLater(() -> {
-            if (users != null && loggedInUser != null && !loggedInUser.getUsername().equals(message.getUsername()))
+            if (users != null && loggedInUser != null && !loggedInUser.getUsername()
+                                                                      .equals(message.getUsername())) {
                 users.add(message.getUsername());
+            }
         });
     }
 
     /**
      * Handles new logged out users
-     *
+     * <p>
      * If a new UserLoggedOutMessage object is posted to the EventBus the name of the newly
      * logged out user is removed from the user list in the main menu.
      * Furthermore if the LOG-Level is set to DEBUG the message "User {@literal
@@ -99,13 +112,13 @@ public class MainMenuPresenter extends AbstractPresenter {
      */
     @Subscribe
     public void onUserLoggedOutMessage(UserLoggedOutMessage message) {
-        LOG.debug("User {}  logged out.",  message.getUsername() );
+        LOG.debug("User {}  logged out.", message.getUsername());
         Platform.runLater(() -> users.remove(message.getUsername()));
     }
 
     /**
      * Handles new list of users
-     *
+     * <p>
      * If a new AllOnlineUsersResponse object is posted to the EventBus the names
      * of currently logged in users are put onto the user list in the main menu.
      * Furthermore if the LOG-Level is set to DEBUG the message "Update of user
@@ -124,15 +137,15 @@ public class MainMenuPresenter extends AbstractPresenter {
 
     /**
      * Updates the main menus user list according to the list given
-     *
+     * <p>
      * This method clears the entire user list and then adds the name of each user
      * in the list given to the main menus user list. If there ist no user list
      * this it creates one.
      *
-     * @implNote The code inside this Method has to run in the JavaFX-application
-     * thread. Therefore it is crucial not to remove the {@code Platform.runLater()}
      * @param userList A list of UserDTO objects including all currently logged in
      *                 users
+     * @implNote The code inside this Method has to run in the JavaFX-application
+     * thread. Therefore it is crucial not to remove the {@code Platform.runLater()}
      * @see de.uol.swp.common.user.UserDTO
      * @since 2019-08-29
      */
@@ -150,7 +163,7 @@ public class MainMenuPresenter extends AbstractPresenter {
 
     /**
      * Method called when the create lobby button is pressed
-     *
+     * <p>
      * If the create lobby button is pressed, this method requests the lobby service
      * to create a new lobby. Therefore it currently uses the lobby name "test"
      * and an user called "ich"
@@ -166,7 +179,7 @@ public class MainMenuPresenter extends AbstractPresenter {
 
     /**
      * Method called when the join lobby button is pressed
-     *
+     * <p>
      * If the join lobby button is pressed, this method requests the lobby service
      * to join a specified lobby. Therefore it currently uses the lobby name "test"
      * and an user called "ich"
@@ -180,5 +193,80 @@ public class MainMenuPresenter extends AbstractPresenter {
         lobbyService.joinLobby("test", new UserDTO("ich", ""));
     }
 
+    /**
+     * Handles the event when the current games button is pressed.
+     * <p>
+     * Posts a ShowCurrentGamesViewEvent to the EventBus to switch the current screen
+     * to the current games view.
+     *
+     * @param event The ActionEvent created by pressing the current games button
+     */
+    @FXML
+    void onCurrentGamesButtonPressed(ActionEvent event) {
+        eventBus.post(showCurrentGamesViewMessage);
+    }
 
+    /**
+     * Handles the event when the join lobby button is pressed.
+     * <p>
+     * Posts a ShowLobbyOverviewViewEvent to the EventBus to switch the current screen
+     * to the lobby overview view.
+     *
+     * @param event The ActionEvent created by pressing the join lobby button
+     */
+    @FXML
+    void onJoinLobbyButtonPressed(ActionEvent event) {
+        eventBus.post(showLobbyOverviewViewMessage);
+    }
+
+    /**
+     * Handles the event when the create lobby button is pressed.
+     * <p>
+     * Posts a CreateLobbyEvent to the EventBus to initiate the creation of a new lobby.
+     *
+     * @param event The ActionEvent created by pressing the create lobby button
+     */
+    @FXML
+    void onCreateLobbyButtonPressed(ActionEvent event) {
+        lobbyService.createNewLobby(
+                "Lobby" + loggedInUser.getUsername(),
+                new UserDTO(loggedInUser.getUsername(), loggedInUser.getPassword())
+        );
+    }
+
+    /**
+     * Handles the event when the rules button is pressed.
+     * <p>
+     * Posts a DownloadRulesEvent to the EventBus to initiate the download of the game rules.
+     *
+     * @param event The ActionEvent created by pressing the rules button
+     */
+    @FXML
+    void onRulesButtonPressed(ActionEvent event) throws UnsopportedMethodExeption {
+        String url = "https://www.brettspielversand" + ".de/mediafiles/spieleanleitungen/zman/114-0021_Pandemic_Iberia_Anleitung.pdf";
+        if (Desktop.isDesktopSupported() && Desktop.getDesktop()
+                                                   .isSupported(Desktop.Action.BROWSE)) {
+            try {
+                Desktop.getDesktop()
+                       .browse(new URI(url));
+            } catch (IOException | URISyntaxException e) {
+                e.printStackTrace();
+            }
+        } else {
+            throw new UnsopportedMethodExeption("Desktop browsing not supported.");
+        }
+    }
+
+    /**
+     * Handles the event when the options button is pressed.
+     * <p>
+     * Posts a ShowOptionViewEvent to the EventBus to switch the current screen
+     * to the options view.
+     *
+     * @param event The ActionEvent created by pressing the options button
+     */
+    @FXML
+    void onOptionsButtonPressed(ActionEvent event) {
+        eventBus.post(showOptionViewMessage);
+    }
 }
---
+++ common/src/main/java/de/uol/swp/common/exception/UnsopportedMethodExeption.java
@@ -0,0 +1,7 @@
+package de.uol.swp.common.exception;
+
+public class UnsopportedMethodExeption extends Exception {
+    public UnsopportedMethodExeption(String message) {
+        super(message);
+    }
+}
