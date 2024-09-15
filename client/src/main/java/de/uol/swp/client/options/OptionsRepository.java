ed074efe9fcad5a83631f3ebf6e88bb22823be42
larshemmers <lars.hemmers@gmail.com>
2024-09-15 12:20:49 +0200

    added save properties

--- client/src/main/java/de/uol/swp/client/options/OptionsRepository.java
+++ client/src/main/java/de/uol/swp/client/options/OptionsRepository.java
@@ -1,8 +1,11 @@
 package de.uol.swp.client.options;
 
 import lombok.Getter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Objects;
 import java.util.Properties;
@@ -15,6 +18,8 @@ public class OptionsRepository {
 
     private static final double DEFAULT_VOLUME = 100.0;
     private static final boolean DEFAULT_CHAT_ENABLED = true;
+
+    private static final Logger LOG = LogManager.getLogger(OptionsRepository.class);
 
     private Properties properties;
     private boolean chatEnabled;
@@ -51,6 +56,12 @@ public class OptionsRepository {
     public void setChatEnabled(boolean chatEnabled) {
         this.chatEnabled = chatEnabled;
         properties.setProperty(CHAT_ENABLED_IDENTIFIER, String.valueOf(chatEnabled));
+
+        try {
+            saveProperties();
+        } catch (IOException e) {
+            LOG.debug("Failed to save properties to file: {}", e.getMessage());
+        }
     }
 
     /**
@@ -61,6 +72,12 @@ public class OptionsRepository {
     public void setVolume(double volume) {
         this.volume = volume;
         properties.setProperty(VOLUME_IDENTIFIER, String.valueOf(volume));
+
+        try {
+            saveProperties();
+        } catch (IOException e) {
+            LOG.debug("Failed to save properties to file: {}", e.getMessage());
+        }
     }
 
     /**
@@ -84,6 +101,22 @@ public class OptionsRepository {
         // Load the properties from the file input stream
         try (FileInputStream fis = new FileInputStream(path)) {
             properties.load(fis);
+        }
+    }
+
+    private void saveProperties() throws IOException {
+        // Get the path to the resource directory
+        String resourcePath = Objects.requireNonNull(Thread.currentThread()
+                                                           .getContextClassLoader()
+                                                           .getResource(""))
+                                     .getPath();
+
+        // Construct the full path to the options file
+        String path = resourcePath + OPTIONS_FILE;
+
+        // Save the properties to the file output stream
+        try (FileOutputStream fos = new FileOutputStream(path)) {
+            properties.store(fos, null);
         }
     }
 }
