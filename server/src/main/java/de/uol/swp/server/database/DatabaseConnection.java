01bb864c6ede976b44b7beea505c7573980d0907
larshemmers <lars.hemmers@gmail.com>
2024-09-15 15:51:05 +0200

    reverted password change

--- server/src/main/java/de/uol/swp/server/database/DatabaseConnection.java
+++ server/src/main/java/de/uol/swp/server/database/DatabaseConnection.java
@@ -13,7 +13,7 @@ public class DatabaseConnection {
     // Datenbankverbindungsinformationen
     private final String jdbcUrl = "jdbc:mysql://localhost:3306/iberia_db";
     private final String username = "root";
-    private final String password = "123456";
+    private final String password = "iberia2024";
 
     // Privater Konstruktor, um eine Instanz zu verhindern
     private DatabaseConnection() throws SQLException {
