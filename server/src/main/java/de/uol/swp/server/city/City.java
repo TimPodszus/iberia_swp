b2c5d399819e231b8022fd4e09b9ac2720b9ca96
larshemmers <lars.hemmers@gmail.com>
2024-09-20 14:56:22 +0200

    added dto conversion in city object

--- server/src/main/java/de/uol/swp/server/city/City.java
+++ server/src/main/java/de/uol/swp/server/city/City.java
@@ -1,5 +1,7 @@
 package de.uol.swp.server.city;
 
+import de.uol.swp.common.city.CityDTO;
+import de.uol.swp.common.city.ICityDTO;
 import de.uol.swp.server.plague.PlagueName;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,12 +11,36 @@ import lombok.Setter;
 @AllArgsConstructor
 @RequiredArgsConstructor
 @Getter
-public class City
-{
+public class City {
     private final PlagueName plagueName;
     private final CityName name;
     private final int foundationDate;
     private final boolean isHarbourCity;
     @Setter
     private boolean hasHospital;
+
+    /**
+     * Converts this City object to a Data Transfer Object (DTO).
+     *
+     * @return an instance of ICityDTO representing this City.
+     */
+    public ICityDTO toDto() {
+        return new CityDTO(name.getDisplayName(), plagueName.name(), foundationDate, isHarbourCity, hasHospital);
+    }
+
+    /**
+     * Creates a City object from a Data Transfer Object (DTO).
+     *
+     * @param dto the Data Transfer Object representing a City.
+     * @return a new City instance based on the provided DTO.
+     */
+    public static City fromDto(ICityDTO dto) {
+        return new City(
+                PlagueName.valueOf(dto.getPlagueName()),
+                CityName.fromDisplayName(dto.getName()),
+                dto.getFoundationDate(),
+                dto.isHarbourCity(),
+                dto.isHospitalBuild()
+        );
+    }
 }
--- server/src/main/java/de/uol/swp/server/city/CityName.java
+++ server/src/main/java/de/uol/swp/server/city/CityName.java
@@ -5,27 +5,42 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public enum CityName
-{
+public enum CityName {
     // Yellow Fever
     ALBACETE("Albacete"), ZARAGOZA("Zaragoza"), ALICANTE("Alicante"), CUENCA("Cuenca"), BARCELONA("Barcelona"), ANDORRA_LA_VELLA(
-        "Andorra la Vella"), TARRAGONA("Tarragona"), VALENCIA("Valencia"), CARTAGENA("Cartagena"), PALMA_DE_MALLORCA(
-        "Palma de Mallorca"), TERUEL("Teruel"), GIRONA("Girona"),
+            "Andorra la Vella"), TARRAGONA("Tarragona"), VALENCIA("Valencia"), CARTAGENA("Cartagena"), PALMA_DE_MALLORCA(
+            "Palma de Mallorca"), TERUEL("Teruel"), GIRONA("Girona"),
 
     // Cholera
     EVORA("Evora"), LISBOA("Lisboa"), COIMBRA("Coimbra"), CACERES("Caceres"), ALBUFEIRA("Albufeira"), BRAGA("Braga"), OURENSE(
-        "Ourense"), SALAMANCA("Salamanca"), VIGO("Vigo"), PORTO("Porto"), A_CORUNA("A Coruna"), SANTIAGO_DE_COMPOSTELA(
-        "Santiago de Compostela"),
+            "Ourense"), SALAMANCA("Salamanca"), VIGO("Vigo"), PORTO("Porto"), A_CORUNA("A Coruna"), SANTIAGO_DE_COMPOSTELA(
+            "Santiago de Compostela"),
 
     // Malaria
     ALMERIA("Almeria"), HUELVA("Huelva"), GRANADA("Granada"), BADAJOZ("Badajoz"), GIBRALTAR("Gibraltar"), MALAGA(
-        "Malaga"), TOLEDO("Toledo"), CADIZ("Cadiz"), CORDOBA("Cordoba"), SEVILLA("Sevilla"), JAEN("Jaen"), CIUDAD_REAL(
-        "Ciudad Real"),
+            "Malaga"), TOLEDO("Toledo"), CADIZ("Cadiz"), CORDOBA("Cordoba"), SEVILLA("Sevilla"), JAEN("Jaen"), CIUDAD_REAL(
+            "Ciudad Real"),
 
     // Typhus
     SAN_SEBASTIAN_DONOSTIA("San Sebastian-Donostia"), SANTANDER("Santander"), GIJON("Gijon"), SORIA("Soria"), BURGOS(
-        "Burgos"), VICTORIA_GASTEIZ("Victoria-Gasteiz"), PAMPLONA("Pamplona"), MADRID("Madrid"), HUESCA("Huesca"), LEON(
-        "Leon"), BILBAO_BILBO("Bilbao-Bilbo"), VALLADOLID("Valladolid");
+            "Burgos"), VICTORIA_GASTEIZ("Victoria-Gasteiz"), PAMPLONA("Pamplona"), MADRID("Madrid"), HUESCA("Huesca"), LEON(
+            "Leon"), BILBAO_BILBO("Bilbao-Bilbo"), VALLADOLID("Valladolid");
 
     private final String displayName;
+
+    /**
+     * Returns the CityName enum constant with the specified display name.
+     *
+     * @param displayName the display name of the city
+     * @return the CityName enum constant with the specified display name, or null if no such constant exists
+     */
+    public static CityName fromDisplayName(String displayName) {
+        for (CityName cityName : CityName.values()) {
+            if (cityName.getDisplayName()
+                        .equals(displayName)) {
+                return cityName;
+            }
+        }
+        return null;
+    }
 }
