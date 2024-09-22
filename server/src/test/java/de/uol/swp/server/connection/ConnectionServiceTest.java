0ec3f48f35b7ef430b5b5cce044d160a6f446859
larshemmers <lars.hemmers@gmail.com>
2024-09-22 11:09:06 +0200

    fixed service test

--- server/src/test/java/de/uol/swp/server/connection/ConnectionServiceTest.java
+++ server/src/test/java/de/uol/swp/server/connection/ConnectionServiceTest.java
@@ -12,7 +12,8 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 
-class ConnectionServiceTest extends EventBusBasedTest {
+// disabled sonar lint rule java:S5786, because public class modifier is required for EventBusBasedTest
+public class ConnectionServiceTest extends EventBusBasedTest {
 
     final ConnectionService connectionService = new ConnectionService(getBus(), new ConnectionManagement());
 
@@ -21,12 +22,17 @@ class ConnectionServiceTest extends EventBusBasedTest {
         handleEvent(e);
     }
 
+    /**
+     * Tests the handling of an AvailableDestinationsRequest.
+     *
+     * @throws InterruptedException if the thread is interrupted while waiting for the event
+     */
     @Test
     void testOnAvailableDestinationsRequest() throws InterruptedException {
         ICityDTO city = new City(PlagueName.YELLOW_FEVER, CityName.PALMA_DE_MALLORCA, -123, true).toDto();
         AvailableDestinationsRequest request = new AvailableDestinationsRequest(city);
         postAndWait(request);
 
-        assertInstanceOf(AvailableDestinationsResponse.class, event);
+        assertInstanceOf(AvailableDestinationsResponse.class, event, "Expected an AvailableDestinationsResponse");
     }
 }
