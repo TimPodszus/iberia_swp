e7ff4cc221518978616d07c1121695c75361b52e
larshemmers <lars.hemmers@gmail.com>
2024-09-23 16:54:23 +0200

    added equals and hash methods to citydto

--- common/src/main/java/de/uol/swp/common/city/CityDTO.java
+++ common/src/main/java/de/uol/swp/common/city/CityDTO.java
@@ -14,4 +14,22 @@ public class CityDTO implements ICityDTO {
     private final int foundationDate;
     private final boolean harbourCity;
     private boolean hospitalBuild;
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+
+        if (object instanceof CityDTO cityDTO) {
+            return name.equals(cityDTO.getName()) && plagueName.equals(cityDTO.getPlagueName()) && foundationDate == cityDTO.getFoundationDate() && harbourCity == cityDTO.isHarbourCity() && hospitalBuild == cityDTO.isHospitalBuild();
+        }
+
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode() + plagueName.hashCode() + foundationDate + (harbourCity ? 1 : 0) + (hospitalBuild ? 1 : 0);
+    }
 }
