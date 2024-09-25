e3c999e4d4e008e4e7e2de6b04fe2205d9ec8480
marten.wempe <marten.wempe@vierol.de>
2024-09-25 21:17:17 +0200

    #64 added gameScreen fxml and Presenter

--- client/src/main/java/de/uol/swp/client/game/GameScreenPresenter.java
+++ client/src/main/java/de/uol/swp/client/game/GameScreenPresenter.java
@@ -12,6 +12,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
