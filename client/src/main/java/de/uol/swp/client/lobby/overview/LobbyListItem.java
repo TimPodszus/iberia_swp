b32ab4a46bfcfc2640766beb610b0be787537414
larshemmers <lars.hemmers@gmail.com>
2024-09-25 22:17:24 +0200

    showing list items in table

--- client/src/main/java/de/uol/swp/client/lobby/overview/LobbyListItem.java
+++ client/src/main/java/de/uol/swp/client/lobby/overview/LobbyListItem.java
@@ -2,17 +2,44 @@ package de.uol.swp.client.lobby.overview;
 
 import lombok.Getter;
 
-@Getter
+
 public class LobbyListItem {
+
+    private static final String MAX_PLAYERS = "/4";
+    private static final String MAX_DIFFICULTY = "/10";
+    private static final String PUBLIC_ACCESS = "Öffentlich";
+    private static final String PRIVATE_ACCESS = "Privat";
+
+    @Getter
     private final String name;
     private final int players;
     private final int difficulty;
 
-    private final boolean access = false;
+    private final boolean privateAccess;
+
+    public LobbyListItem(String name, int players, int difficulty, boolean privateAccess) {
+        this.name = name;
+        this.players = players;
+        this.difficulty = difficulty;
+        this.privateAccess = privateAccess;
+    }
 
     public LobbyListItem(String name, int players, int difficulty) {
         this.name = name;
         this.players = players;
         this.difficulty = difficulty;
+        this.privateAccess = true;
+    }
+
+    public String getPlayers() {
+        return players + MAX_PLAYERS;
+    }
+
+    public String getDifficulty() {
+        return difficulty + MAX_DIFFICULTY;
+    }
+
+    public String getAccess() {
+        return privateAccess ? PRIVATE_ACCESS : PUBLIC_ACCESS;
     }
 }
--- client/src/main/java/de/uol/swp/client/lobby/overview/LobbyOverviewPresenter.java
+++ client/src/main/java/de/uol/swp/client/lobby/overview/LobbyOverviewPresenter.java
@@ -2,15 +2,20 @@ package de.uol.swp.client.lobby.overview;
 
 import com.google.inject.Inject;
 import de.uol.swp.client.AbstractPresenter;
+import de.uol.swp.client.SceneManager;
 import de.uol.swp.client.lobby.LobbyService;
 import de.uol.swp.client.main.event.ShowLastSceneEvent;
 import de.uol.swp.common.lobby.ILobby;
 import de.uol.swp.common.lobby.response.LobbyListResponse;
 import de.uol.swp.common.user.UserDTO;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.greenrobot.eventbus.Subscribe;
 
 import java.util.ArrayList;
@@ -19,8 +24,9 @@ import java.util.Map;
 
 public class LobbyOverviewPresenter extends AbstractPresenter {
     public static final String FXML = "/fxml/LobbyOverviewView.fxml";
+    private static final Logger LOG = LogManager.getLogger(SceneManager.class);
 
-    public static final Map<String, ILobby> lobbyList = Map.of();
+    private Map<String, ILobby> lobbyList = Map.of();
 
     @Inject
     private LobbyService lobbyService;
@@ -36,13 +42,41 @@ public class LobbyOverviewPresenter extends AbstractPresenter {
     }
 
     /**
-     * Initializes the presenter by setting up the search input listener
-     * and requesting the lobby list from the lobby service.
+     * Initializes the presenter by setting up the search input listener, requesting the lobby list from the lobby
+     * service and setting up the lobby table.
      */
     @FXML
     private void initialize() {
         searchInput.textProperty()
                    .addListener((observable, oldValue, newValue) -> filterLobbies(newValue));
+
+        lobbyTable.getColumns()
+                  .get(0)
+                  .setCellValueFactory(new PropertyValueFactory<>("name"));
+        lobbyTable.getColumns()
+                  .get(1)
+                  .setCellValueFactory(new PropertyValueFactory<>("players"));
+        lobbyTable.getColumns()
+                  .get(2)
+                  .setCellValueFactory(new PropertyValueFactory<>("difficulty"));
+        lobbyTable.getColumns()
+                  .get(3)
+                  .setCellValueFactory(new PropertyValueFactory<>("access"));
+        lobbyTable.setPlaceholder(new Label("Keine Lobby gefunden"));
+        lobbyTable.setRowFactory(tv -> {
+            TableRow<LobbyListItem> row = new TableRow<>();
+            row.setOnMouseClicked(event -> {
+                if (event.getClickCount() == 2 && (!row.isEmpty())) {
+                    LOG.debug("Joining lobby: " + row.getItem()
+                                                     .getName());
+                    LobbyListItem rowData = row.getItem();
+                    //TODO get user from login
+                    lobbyService.joinLobby(rowData.getName(), new UserDTO("ich", ""));
+                }
+            });
+            return row;
+        });
+
         lobbyService.requestLobbyList();
     }
 
@@ -54,10 +88,8 @@ public class LobbyOverviewPresenter extends AbstractPresenter {
      */
     @Subscribe
     public void onLobbyListMessage(LobbyListResponse message) {
-        setLobbyList(message.getLobbies()
-                            .values()
-                            .stream()
-                            .toList());
+        this.lobbyList = message.getLobbies();
+        setLobbyList(new ArrayList<>(this.lobbyList.values()));
     }
 
     /**
@@ -83,8 +115,13 @@ public class LobbyOverviewPresenter extends AbstractPresenter {
      * @param searchInputText the text to filter the lobbies by
      */
     private void filterLobbies(String searchInputText) {
+        if (searchInputText == null || searchInputText.isEmpty()) {
+            setLobbyList(new ArrayList<>(this.lobbyList.values()));
+            return;
+        }
+
         List<ILobby> filteredLobbies = new ArrayList<>();
-        for (ILobby lobby : lobbyList.values()) {
+        for (ILobby lobby : this.lobbyList.values()) {
             if (lobby.getName()
                      .contains(searchInputText) || lobby.getLobbyCode()
                                                         .contains(searchInputText)) {
@@ -113,17 +150,5 @@ public class LobbyOverviewPresenter extends AbstractPresenter {
                   .clear();
         lobbyTable.getItems()
                   .addAll(lobbyListItems);
-
-        lobbyTable.setRowFactory(tv -> {
-            TableRow<LobbyListItem> row = new TableRow<>();
-            row.setOnMouseClicked(event -> {
-                if (event.getClickCount() == 2 && (!row.isEmpty())) {
-                    LobbyListItem rowData = row.getItem();
-                    //TODO get user from login
-                    lobbyService.joinLobby(rowData.getName(), new UserDTO("ich", ""));
-                }
-            });
-            return row;
-        });
     }
 }
