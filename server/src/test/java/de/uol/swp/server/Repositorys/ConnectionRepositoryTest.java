dbeac2f9c706105735a68164ba546b6f20c004b2
Kristian Tapken <kristian.tapken@amcon.de>
2024-09-27 17:14:43 +0200

    add UnitTests for Repositories

---
+++ server/src/test/java/de/uol/swp/server/Repositorys/ConnectionRepositoryTest.java
@@ -0,0 +1,41 @@
+package de.uol.swp.server.Repositorys;
+
+import de.uol.swp.server.city.CityName;
+import de.uol.swp.server.connection.Connection;
+import de.uol.swp.server.connection.ConnectionRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConnectionRepositoryTest {
+    static ConnectionRepository repository;
+    static List<Connection> connections;
+
+    @BeforeAll
+    static void create() {
+        repository = new ConnectionRepository();
+        connections = repository.getConnections();
+    }
+
+
+    @Test
+    void testCreateAllConnections() {
+        assertEquals(81, connections.size());
+    }
+
+    @Test
+    void testGetConnectionByID() {
+        Connection connection1 = connections.get(0);
+        assertEquals(1, connection1.getId());
+        assertTrue(connection1.isTrainTrackBuildable());
+        assertEquals(List.of(CityName.A_CORUNA, CityName.GIJON), connection1.getCityNames());
+
+        Connection connection28 = connections.get(27);
+        assertEquals(28, connection28.getId());
+        assertFalse(connection28.isTrainTrackBuildable());
+        assertEquals(List.of(CityName.CADIZ, CityName.GIBRALTAR), connection28.getCityNames());
+    }
+}
---
+++ server/src/test/java/de/uol/swp/server/Repositorys/RegionRepositoryTest.java
@@ -0,0 +1,35 @@
+package de.uol.swp.server.Repositorys;
+
+import de.uol.swp.server.city.CityRepository;
+import de.uol.swp.server.region.Region;
+import de.uol.swp.server.region.RegionRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class RegionRepositoryTest {
+    static RegionRepository repository;
+    static List<Region> regions;
+
+    @BeforeAll
+    static void create() {
+        CityRepository cityRepository = new CityRepository();
+        repository = new RegionRepository(cityRepository);
+        regions = repository.getRegions();
+    }
+
+    @Test
+    void testGetAllRegions() {
+        assertEquals(34, regions.size());
+    }
+
+    @Test
+    void testGetRegionByID() {
+        Region region = repository.getRegionByID(1);
+        assertNotNull(region);
+    }
+}
