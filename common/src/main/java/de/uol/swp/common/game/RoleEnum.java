c30552380d5c4534155218764c39fdc5cf9c4769
marten.wempe <marten.wempe@vierol.de>
2024-10-09 17:06:11 +0200

    #115 adapt RoleEnum

--- common/src/main/java/de/uol/swp/common/game/RoleEnum.java
+++ common/src/main/java/de/uol/swp/common/game/RoleEnum.java
@@ -1,20 +1,19 @@
 package de.uol.swp.common.game;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@AllArgsConstructor
 @Getter
 public enum RoleEnum {
-    AGRICULTURAL_SCIENTIST("#60BC46"),
-    POLITICIAN("#F1D61B"),
-    RAILWAY_PERSON("#FF0000"),
-    SAILOR("#1DA6C3"),
-    SCIENTIST_OF_THE_ROYAL_ACADEMY("#FF0000"),
-    NURSE("#D44446"),
-    COUNTRY_DOCTOR("#CE74A8");
+    AGRICULTURAL_SCIENTIST("#60BC46", "Agrarwissenschaftler"),
+    POLITICIAN("#F1D61B", "Politiker"),
+    RAILWAY_PERSON("#FF0000", "Eisenbahner"),
+    SAILOR("#1DA6C3", "Seemann"),
+    SCIENTIST_OF_THE_ROYAL_ACADEMY("#FF0000", "Wissenschaftler der Königlichen Akademie"),
+    NURSE("#D44446", "Krankenschwester"),
+    COUNTRY_DOCTOR("#CE74A8", "Landarzt"),;
 
     private final String colorCode;
-
-    RoleEnum(String colorCode) {
-        this.colorCode = colorCode;
-    }
+    private final String name;
 }
