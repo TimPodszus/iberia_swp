e0a7455f0811d70d1ce01ca97e833a4368c469da
marten.wempe <marten.wempe@vierol.de>
2024-10-09 16:38:42 +0200

    #115 adapt PlayerClasses and added missing classes, also mapper

--- common/src/main/java/de/uol/swp/common/player/IPlayerDTO.java
+++ common/src/main/java/de/uol/swp/common/player/IPlayerDTO.java
@@ -2,13 +2,14 @@ package de.uol.swp.common.player;
 
 import de.uol.swp.common.cards.CardDTO;
 import de.uol.swp.common.city.CityDTO;
+import de.uol.swp.common.role.IRoleDTO;
 
 import java.util.List;
 
 public interface IPlayerDTO {
     String getUsername();
 
-    String getRoleName();
+    IRoleDTO getRole();
 
     CityDTO getCurrentPosition();
 
