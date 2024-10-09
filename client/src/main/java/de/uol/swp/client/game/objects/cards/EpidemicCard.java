565480585f9ef93aef672a7a217a5696444232af
marten.wempe <marten.wempe@vierol.de>
2024-10-09 21:02:57 +0200

    #115 renamed EpidemicCard

--- client/src/main/java/de/uol/swp/client/game/objects/cards/EpidemicCard.java
+++ client/src/main/java/de/uol/swp/client/game/objects/cards/EpidemicCard.java
@@ -4,8 +4,8 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
-public class EpedemicCard extends AbstractCard {
-    public EpedemicCard() {
+public class EpidemicCard extends AbstractCard {
+    public EpidemicCard() {
         this.setPrefSize(100, 150);
         this.setStyle("-fx-background-color: f7d48e;");
 
