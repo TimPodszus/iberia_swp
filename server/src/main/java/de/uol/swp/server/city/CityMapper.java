8acb4c606301a475c53f2b32726429fcd644bf2a
marten.wempe <marten.wempe@vierol.de>
2024-10-09 16:39:16 +0200

    #115 adapt CityClasses and added missing classes, also mapper

--- server/src/main/java/de/uol/swp/server/city/CityMapper.java
+++ server/src/main/java/de/uol/swp/server/city/CityMapper.java
@@ -2,21 +2,24 @@ package de.uol.swp.server.city;
 
 import de.uol.swp.common.city.CityDTO;
 import de.uol.swp.common.city.ICityDTO;
+import de.uol.swp.server.infection.InfectionMapper;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
 public class CityMapper {
-
     public static ICityDTO toDTO(City city) {
         return new CityDTO(
-                city.getPlagueName()
-                    .toString(),
+                city.getId(),
+                city.getPlagueName(),
                 city.getName()
                     .toString(),
                 city.getFoundationDate(),
                 city.isHarbourCity(),
-                city.isHospitalBuilt()
+                city.isHospitalBuilt(),
+                InfectionMapper.toDTOList(city.getInfections())
         );
     }
 
@@ -24,13 +27,14 @@ public class CityMapper {
         List<ICityDTO> citiesDto = new ArrayList<>();
         for (City city : cities) {
             CityDTO cityDTO = new CityDTO(
-                    city.getPlagueName()
-                        .toString(),
+                    city.getId(),
+                    city.getPlagueName(),
                     city.getName()
                         .toString(),
                     city.getFoundationDate(),
                     city.isHarbourCity(),
-                    city.isHospitalBuilt()
+                    city.isHospitalBuilt(),
+                    InfectionMapper.toDTOList(city.getInfections())
             );
             citiesDto.add(cityDTO);
         }
