a4c23854897627ac6c884dc4795e5672f0082dab
marten.wempe <marten.wempe@vierol.de>
2024-10-09 21:06:22 +0200

    #115 adapt PlayerButton

--- client/src/main/java/de/uol/swp/client/game/objects/PlayerButton.java
+++ client/src/main/java/de/uol/swp/client/game/objects/PlayerButton.java
@@ -3,16 +3,20 @@ package de.uol.swp.client.game.objects;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 
 public class PlayerButton extends Button {
     public PlayerButton(String player, EventHandler<ActionEvent> eventHandler) {
         this.setText(player);
         this.setMnemonicParsing(false);
         this.setPrefHeight(40.0);
+        this.setPrefWidth(50.0);
         this.getStyleClass()
             .add("action-button");
         this.setUserData(1);
 
         this.setOnAction(eventHandler);
+
+        HBox.setMargin(this, new javafx.geometry.Insets(5.0, 5.0, 5.0, 5.0));
     }
 }
