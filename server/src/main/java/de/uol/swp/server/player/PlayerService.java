24428941311a1ffdc7de69a6ec691828661c3dce
Kristian Tapken <kristian.tapken@amcon.de>
2025-01-09 22:31:35 +0100

    adjust playerManagement and -service

--- server/src/main/java/de/uol/swp/server/player/PlayerService.java
+++ server/src/main/java/de/uol/swp/server/player/PlayerService.java
@@ -6,6 +6,7 @@ import de.uol.swp.common.game.message.response.StatusResponse;
 import de.uol.swp.common.message.response.AbstractResponseMessage;
 import de.uol.swp.common.player.request.DrawPlayerCardRequest;
 import de.uol.swp.common.player.request.DrawPlayerCardResponse;
+import de.uol.swp.common.user.Session;
 import de.uol.swp.server.AbstractService;
 import de.uol.swp.server.game.GameMapper;
 import de.uol.swp.server.game.data.IGame;
@@ -44,7 +45,9 @@ public class PlayerService extends AbstractService {
         IGame game = GameStore.getInstance()
                               .getGame(request.getLobbyCode());
         try {
-            ICardDTO card = playerManagement.drawPlayerCard(game, request);
+            Session session = request.getSession()
+                                     .orElseThrow(() -> new IllegalStateException("Session not present"));
+            ICardDTO card = playerManagement.drawPlayerCard(game, session.getUser());
             response = new DrawPlayerCardResponse(true, "Card drawn successfully", card);
         } catch (PlayerManagementException e) {
             response = new StatusResponse(false, "Error drawing a player card");
--- server/src/main/java/de/uol/swp/server/player/management/IPlayerManagement.java
+++ server/src/main/java/de/uol/swp/server/player/management/IPlayerManagement.java
@@ -1,9 +1,12 @@
 package de.uol.swp.server.player.management;
 
 import de.uol.swp.common.cards.ICardDTO;
-import de.uol.swp.common.player.request.DrawPlayerCardRequest;
+import de.uol.swp.common.user.User;
 import de.uol.swp.server.game.data.IGame;
+import de.uol.swp.server.player.data.Player;
 
 public interface IPlayerManagement {
-    ICardDTO drawPlayerCard(IGame game, DrawPlayerCardRequest request) throws PlayerManagementException;
+    ICardDTO drawPlayerCard(IGame game, User user) throws PlayerManagementException;
+
+    ICardDTO drawPlayerCard(IGame game, Player player) throws PlayerManagementException;
 }
