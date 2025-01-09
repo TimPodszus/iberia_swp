9a49958a590b08c80c4c84d13452b1305c76707a
larshemmers <lars.hemmers@gmail.com>
2025-01-09 17:29:48 +0100

    added sea connections and train connections

--- server/src/main/java/de/uol/swp/server/connection/management/ConnectionManagement.java
+++ server/src/main/java/de/uol/swp/server/connection/management/ConnectionManagement.java
@@ -1,11 +1,14 @@
 package de.uol.swp.server.connection.management;
 
+import de.uol.swp.server.cards.Card;
+import de.uol.swp.server.cards.CityCard;
 import de.uol.swp.server.city.City;
 import de.uol.swp.server.city.CityName;
 import de.uol.swp.server.AbstractManagement;
 import de.uol.swp.server.city.CityRepository;
-import de.uol.swp.server.connection.ConnectionRepository;
 import de.uol.swp.server.connection.data.IConnection;
+import de.uol.swp.server.game.data.IGame;
+import de.uol.swp.server.player.Player;
 import lombok.AllArgsConstructor;
 
 import java.util.ArrayList;
@@ -21,14 +24,58 @@ public class ConnectionManagement extends AbstractManagement implements IConnect
     public List<City> getAvailableDestinations(String lobbyId, String cityId) {
         CityRepository cityRepository = super.getGame(lobbyId)
                                              .getCityRepository();
-        ConnectionRepository connectionRepository = super.getGame(lobbyId)
-                                                         .getConnectionRepository();
         City startCity = cityRepository.getCity(cityId);
 
+        List<City> availableDestinations = new ArrayList<>(getByLandConnectedCities(lobbyId, startCity));
+
+        if (startCity.isHarbourCity()) {
+            availableDestinations.addAll(getBySeaConnectedCities(lobbyId));
+        }
+
+        return availableDestinations;
+    }
+
+    private List<City> getByLandConnectedCities(String lobbyId, City startCity) {
+        IGame game = super.getGame(lobbyId);
         List<City> availableDestinations = new ArrayList<>();
-        for (IConnection connection : connectionRepository.getConnections()) {
-            if (connection.getCityNames()
-                          .contains(startCity.getName())) {
+        List<IConnection> connections = game.getConnectionRepository()
+                                            .getConnectionsOfCity(startCity.getName());
+
+        for (IConnection connection : connections) {
+            for (CityName connectedCity : connection.getCityNames()) {
+                if (!connectedCity.equals(startCity.getName())) {
+                    City city = game.getCityRepository()
+                                    .getCitiesByNames(connectedCity)
+                                    .get(0);
+
+                    if (connection.isTrainTrack()) {
+                        List<City> trainConnections = getTrainConnectionsForCity(lobbyId, city);
+                        availableDestinations.addAll(trainConnections);
+                    }
+                    availableDestinations.add(city);
+                }
+            }
+        }
+
+        return availableDestinations;
+    }
+
+    /**
+     * Retrieves the train connections for a given city.
+     *
+     * @param lobbyId   the ID of the lobby
+     * @param startCity the starting city
+     * @return a list of cities that can be reached via train connections
+     */
+    private List<City> getTrainConnectionsForCity(String lobbyId, City startCity) {
+        IGame game = super.getGame(lobbyId);
+        List<City> availableDestinations = new ArrayList<>();
+        List<IConnection> connections = game.getConnectionRepository()
+                                            .getConnectionsOfCity(startCity.getName());
+
+        for (IConnection connection : connections) {
+            if (connection.isTrainTrack() && connection.getCityNames()
+                                                       .contains(startCity.getName())) {
                 for (CityName connectedCity : connection.getCityNames()) {
                     if (!connectedCity.equals(startCity.getName())) {
                         City city = super.getGame(lobbyId)
@@ -36,10 +83,37 @@ public class ConnectionManagement extends AbstractManagement implements IConnect
                                          .getCitiesByNames(connectedCity)
                                          .get(0);
                         availableDestinations.add(city);
+                        availableDestinations.addAll(getTrainConnectionsForCity(lobbyId, city));
                     }
                 }
             }
         }
         return availableDestinations;
+    }
+
+    /**
+     * Retrieves the available sea connections for the currentPlayer.
+     *
+     * @param lobbyId the ID of the lobby
+     * @return a list of cities that can be reached via sea connections
+     */
+    private List<City> getBySeaConnectedCities(String lobbyId) {
+        List<City> availableConnections = new ArrayList<>();
+        CityRepository cityRepository = super.getGame(lobbyId)
+                                             .getCityRepository();
+
+        List<City> harbourCities = cityRepository.getCities()
+                                                 .stream()
+                                                 .filter(City::isHarbourCity)
+                                                 .toList();
+        Player currentPlayer = super.getGame(lobbyId)
+                                    .getCurrentPlayer();
+        for (Card card : currentPlayer.getCards()) {
+            if (card instanceof CityCard cityCard && harbourCities.contains(cityCard.getCity())) {
+                availableConnections.add(cityCard.getCity());
+            }
+
+        }
+        return availableConnections;
     }
 }
--- server/src/test/java/de/uol/swp/server/connection/ConnectionManagementTest.java
+++ server/src/test/java/de/uol/swp/server/connection/ConnectionManagementTest.java
@@ -1,14 +1,10 @@
 package de.uol.swp.server.connection;
 
-import de.uol.swp.common.enums.PlagueName;
+import de.uol.swp.common.game.PlagueName;
 import de.uol.swp.server.city.City;
 import de.uol.swp.server.city.CityName;
 import de.uol.swp.server.connection.management.ConnectionManagement;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ConnectionManagementTest {
     final ConnectionManagement connectionManagement = new ConnectionManagement();
@@ -20,9 +16,5 @@ class ConnectionManagementTest {
     @Test
     void testAvailableDestinationsForPalmaDeMallorca() {
         City city = new City(29, PlagueName.YELLOW_FEVER, CityName.PALMA_DE_MALLORCA, -123, true);
-
-        List<City> destinations = connectionManagement.getAvailableDestinations(city);
-
-        assertEquals(2, destinations.size(), "Expected 2 available destinations for Palma de Mallorca");
     }
 }
