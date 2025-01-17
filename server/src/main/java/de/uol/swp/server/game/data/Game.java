fb969b8b89353cc60bf117ca47a980dcc621056f
larshemmers <lars.hemmers@gmail.com>
2025-01-17 09:53:25 +0100

    removed type from constructor parameters for cards. Set it to the default

--- server/src/main/java/de/uol/swp/server/game/data/Game.java
+++ server/src/main/java/de/uol/swp/server/game/data/Game.java
@@ -1,6 +1,5 @@
 package de.uol.swp.server.game.data;
 
-import de.uol.swp.common.cards.CardType;
 import de.uol.swp.server.cards.Card;
 import de.uol.swp.server.cards.CityCard;
 import de.uol.swp.server.cards.EpidemicCard;
@@ -12,7 +11,7 @@ import de.uol.swp.server.connection.ConnectionRepository;
 import de.uol.swp.server.game.management.GameManagement;
 import de.uol.swp.server.game.states.IGameState;
 import de.uol.swp.server.game.states.StartState;
-import de.uol.swp.server.plague.PlagueRepository;
+import de.uol.swp.server.plague.data.PlagueRepository;
 import de.uol.swp.server.player.data.IPlayer;
 import de.uol.swp.server.player.management.PlayerManagement;
 import de.uol.swp.server.region.RegionRepository;
@@ -187,7 +186,6 @@ public class Game implements IGame {
                     i,
                     city.getName()
                         .toString(),
-                    CardType.INFECTION_CARD,
                     city
             );
             infectionCardDrawPile.add(infectionCard);
@@ -210,7 +208,7 @@ public class Game implements IGame {
     }
 
     public EpidemicCard createEpidemicCard(int id) {
-        return new EpidemicCard(id, "Epidemiekarte", CardType.EPIDEMIC_CARD, "");
+        return new EpidemicCard(id, "Epidemiekarte", "");
     }
 
     public void gameStartShuffle(int numSubDecks) {
