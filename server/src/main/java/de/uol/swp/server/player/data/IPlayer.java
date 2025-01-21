35dc2838277f62f76142e0b882481b5397504384
larshemmers <lars.hemmers@gmail.com>
2025-01-21 18:56:29 +0100

    added getcitycard to player

--- server/src/main/java/de/uol/swp/server/player/data/IPlayer.java
+++ server/src/main/java/de/uol/swp/server/player/data/IPlayer.java
@@ -1,6 +1,7 @@
 package de.uol.swp.server.player.data;
 
 import de.uol.swp.server.cards.Card;
+import de.uol.swp.server.cards.CityCard;
 import de.uol.swp.server.city.CityRepository;
 import de.uol.swp.server.city.data.ICity;
 import de.uol.swp.server.role.IRole;
@@ -85,4 +86,12 @@ public interface IPlayer {
      * @param card the card to discard
      */
     void discardCard(Card card);
+
+    /**
+     * Gets the city card of the player. Returns null if the player does not have the city card.
+     *
+     * @param city the city to get the card for
+     * @return the city card or null, if the player does not have the card
+     */
+    CityCard getCityCard(ICity city);
 }
--- server/src/main/java/de/uol/swp/server/player/data/Player.java
+++ server/src/main/java/de/uol/swp/server/player/data/Player.java
@@ -1,6 +1,7 @@
 package de.uol.swp.server.player.data;
 
 import de.uol.swp.server.cards.Card;
+import de.uol.swp.server.cards.CityCard;
 import de.uol.swp.server.city.CityRepository;
 import de.uol.swp.server.city.data.ICity;
 import de.uol.swp.server.role.IRole;
@@ -45,6 +46,16 @@ public class Player implements IPlayer {
 
         //Für Unittest:
         cards.remove(card);
+    }
+
+    public CityCard getCityCard(ICity city) {
+        for (Card card : cards) {
+            if (card instanceof CityCard cityCard && cityCard.getCity()
+                                                             .equals(city)) {
+                return cityCard;
+            }
+        }
+        return null;
     }
 }
 
