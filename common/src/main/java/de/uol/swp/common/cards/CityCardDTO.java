7fa04ca1bd207cc06e910b7fc6b7fdde9860646d
larshemmers <lars.hemmers@gmail.com>
2025-01-23 15:35:29 +0100

    added messages

--- common/src/main/java/de/uol/swp/common/cards/CityCardDTO.java
+++ common/src/main/java/de/uol/swp/common/cards/CityCardDTO.java
@@ -1,15 +1,15 @@
 package de.uol.swp.common.cards;
 
-import de.uol.swp.common.city.CityDTO;
+import de.uol.swp.common.city.ICityDTO;
 import lombok.Getter;
 
 
 @Getter
 
 public class CityCardDTO extends CardDTO {
-    private final CityDTO city;
+    private final ICityDTO city;
 
-    public CityCardDTO(int id, String title, CardType type, CityDTO city) {
+    public CityCardDTO(int id, String title, CardType type, ICityDTO city) {
         super(id, title, type);
         this.city = city;
     }
