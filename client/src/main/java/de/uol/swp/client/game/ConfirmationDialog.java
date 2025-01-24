46bbb010d841b4bfa093f0002a0e2c4eced3715c
Tim Podszus <tim.podszus@student.ibs-ol.de>
2025-01-24 08:41:57 +0000

    Syntaxfehler

--- client/src/main/java/de/uol/swp/client/game/ConfirmationDialog.java
+++ client/src/main/java/de/uol/swp/client/game/ConfirmationDialog.java
@@ -9,7 +9,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import lombok.NoArgsConstructor
+import lombok.NoArgsConstructor;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
