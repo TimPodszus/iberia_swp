d55f577af12851945e89898e1d957d137bba9225
larshemmers <lars.hemmers@gmail.com>
2025-01-31 18:53:14 +0100

    added username as parameter to moveplayer request

--- common/src/main/java/de/uol/swp/common/player/request/MovePlayerRequest.java
+++ common/src/main/java/de/uol/swp/common/player/request/MovePlayerRequest.java
@@ -1,6 +1,5 @@
 package de.uol.swp.common.player.request;
 
-import de.uol.swp.common.cards.ICardDTO;
 import de.uol.swp.common.game.message.AbstractGameRequest;
 import lombok.Getter;
 
@@ -8,11 +7,25 @@ import java.util.Objects;
 
 @Getter
 public class MovePlayerRequest extends AbstractGameRequest {
+    private final String username;
     private final int cityId;
     private final int cardId;
 
     /**
-     * Constructs a new MovePlayerRequest.
+     * Constructs a new MovePlayerRequest with no card to discard and no user to take with.
+     *
+     * @param lobbyCode the code of the lobby
+     * @param cityId    the ID of the city
+     */
+    public MovePlayerRequest(String lobbyCode, int cityId) {
+        super(lobbyCode);
+        this.cityId = cityId;
+        this.cardId = -1;
+        this.username = "";
+    }
+
+    /**
+     * Constructs a new MovePlayerRequest with no user to take with.
      *
      * @param lobbyCode the code of the lobby
      * @param cityId    the ID of the city
@@ -21,6 +34,22 @@ public class MovePlayerRequest extends AbstractGameRequest {
         super(lobbyCode);
         this.cityId = cityId;
         this.cardId = cardId;
+        this.username = "";
+    }
+
+    /**
+     * Constructs a new MovePlayerRequest.
+     *
+     * @param lobbyCode the code of the lobby
+     * @param cityId    the ID of the city
+     * @param cardId    the ID of the card
+     * @param username  the username of the player, which should be taken with
+     */
+    public MovePlayerRequest(String lobbyCode, int cityId, int cardId, String username) {
+        super(lobbyCode);
+        this.cityId = cityId;
+        this.cardId = cardId;
+        this.username = username;
     }
 
     @Override
