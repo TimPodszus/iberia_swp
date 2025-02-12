ee546e77de1b17c4712bc21369cfcbd2fa08f66d
Felix Siemer <Felix.Siemer@student.ibs-ol.de>
2025-02-12 17:17:05 +0100

    test fix

--- server/src/test/java/de/uol/swp/server/city/CityManagementTest.java
+++ server/src/test/java/de/uol/swp/server/city/CityManagementTest.java
@@ -15,12 +15,15 @@ import de.uol.swp.server.player.management.IPlayerManagement;
 import de.uol.swp.server.region.management.RegionManagement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -42,7 +45,9 @@ public class CityManagementTest {
     private IPlayerManagement playerManagement;
     @Mock
     private RegionManagement regionManagement;
-    private final GameManagement gameManagement = new GameManagement(playerManagement, cityManagement, regionManagement);
+    @InjectMocks
+    private final GameManagement gameManagement = new GameManagement(playerManagement, cityManagement,
+            regionManagement);
     private final InfectionManagement infectionManagement = new InfectionManagement();
 
 
@@ -55,6 +60,7 @@ public class CityManagementTest {
     void setUp() {
         MockitoAnnotations.openMocks(this);
         cityManagement = new CityManagement(regionManagement, gameManagement, infectionManagement);
+        when(regionManagement.reduceWaterTreatments(any(IGame.class), any(ICity.class), anyInt())).thenReturn(1);
     }
 
     /**
@@ -87,17 +93,17 @@ public class CityManagementTest {
         cityManagement.infectCityWithOwnPlague(game, infectionCard, 1);
 
         List<CityName> cityNames = game.getConnectionRepository()
-                     .getCityNamesOfConnectedCitiesByCityName(CityName.BARCELONA);
+                                       .getCityNamesOfConnectedCitiesByCityName(CityName.BARCELONA);
 
         List<ICity> connectedCitys = game.getCityRepository().getCitiesByNames(cityNames);
 
         for (ICity connectedCity : connectedCitys) {
             assertTrue(connectedCity.getInfections()
-                           .stream()
-                           .filter(infection -> infection.getPlagueName() == this.city.getPlagueName())
-                           .findFirst()
-                           .map(infection -> infection.getSeverity() == 1)
-                           .orElse(false));
+                                    .stream()
+                                    .filter(infection -> infection.getPlagueName() == this.city.getPlagueName())
+                                    .findFirst()
+                                    .map(infection -> infection.getSeverity() == 1)
+                                    .orElse(false));
         }
 
         assertEquals(1, game.getEscalationStage());
@@ -136,7 +142,7 @@ public class CityManagementTest {
     @Test
     void testInfectCityWithOwnPlagueWithInvalidParameters() {
         assertThrows(
-            CityManagementException.class, () -> cityManagement.infectCityWithOwnPlague(game, infectionCard, -1));
+                CityManagementException.class, () -> cityManagement.infectCityWithOwnPlague(game, infectionCard, -1));
     }
 
     /**
