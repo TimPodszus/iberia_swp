8d616c00986acb58530a583e3b252f00d03443d8
larshemmers <lars.hemmers@gmail.com>
2025-02-18 12:54:19 +0100

    added card service test

--- server/src/main/java/de/uol/swp/server/cards/CardService.java
+++ server/src/main/java/de/uol/swp/server/cards/CardService.java
@@ -9,6 +9,8 @@ import de.uol.swp.server.AbstractService;
 import de.uol.swp.server.cards.management.ICardManagement;
 import de.uol.swp.server.game.GameException;
 import de.uol.swp.server.game.GameMapper;
+import de.uol.swp.server.lobby.data.ILobby;
+import de.uol.swp.server.lobby.management.ILobbyManagement;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.greenrobot.eventbus.EventBus;
@@ -17,7 +19,15 @@ import org.greenrobot.eventbus.Subscribe;
 public class CardService extends AbstractService {
     private static final Logger LOG = LogManager.getLogger(CardService.class);
 
-    private final ICardManagement cardManagement;
+    /**
+     * For testing purposes these are private and not final
+     */
+    private ICardManagement cardManagement;
+
+    /**
+     * For testing purposes these are private and not final
+     */
+    private ILobbyManagement lobbyManagement;
 
     /**
      * Constructor
@@ -25,9 +35,10 @@ public class CardService extends AbstractService {
      * @param bus the EvenBus used throughout the server
      */
     @Inject
-    public CardService(EventBus bus, ICardManagement cardManagement) {
+    public CardService(EventBus bus, ICardManagement cardManagement, ILobbyManagement lobbyManagement) {
         super(bus);
         this.cardManagement = cardManagement;
+        this.lobbyManagement = lobbyManagement;
     }
 
     @Subscribe
@@ -39,13 +50,14 @@ public class CardService extends AbstractService {
                                    return new GameException("Session missing in PlayCardRequest");
                                })
                                .getUser();
-        
+
         cardManagement.playCard(request.getLobbyId(), user.getUsername(), request.getCardId());
 
         LOG.info("[LobbyId: {}] Played the card. Sending BoardUpdateEvent", request.getLobbyId());
         IGameDTO game = GameMapper.toDTO(cardManagement.getGame(request.getLobbyId()));
         BoardUpdateEvent event = new BoardUpdateEvent(request.getLobbyId(), game);
-        bus.post(event);
+        ILobby lobby = lobbyManagement.getLobby(request.getLobbyId());
+        sendToAllInLobby(lobby, event);
     }
 
 }
