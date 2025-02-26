a10561bde4d96c986effdb271afa5b3522e4915c
Lasse Mollenhauer <lasse.mollenhauer@openknowledge.de>
2025-02-26 18:44:06 +0100

    plagueservice

--- server/src/main/java/de/uol/swp/server/ServerApp.java
+++ server/src/main/java/de/uol/swp/server/ServerApp.java
@@ -12,6 +12,7 @@ import de.uol.swp.server.connection.ConnectionService;
 import de.uol.swp.server.di.ServerModule;
 import de.uol.swp.server.game.GameService;
 import de.uol.swp.server.lobby.LobbyService;
+import de.uol.swp.server.plague.PlagueService;
 import de.uol.swp.server.plague.management.PlagueManagement;
 import de.uol.swp.server.region.RegionService;
 import de.uol.swp.server.player.PlayerService;
@@ -85,7 +86,7 @@ class ServerApp {
         injector.getInstance(CardService.class);
         injector.getInstance(RegionService.class);
         injector.getInstance(CityService.class);
-        injector.getInstance(PlayerService.class);
+        injector.getInstance(PlagueService.class);
     }
 
 }
