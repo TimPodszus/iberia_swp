eca7984f6d33fe0412d1fe4c5aa33a78c24d3dd9
larshemmers <lars.hemmers@gmail.com>
2025-02-26 22:52:49 +0100

    added getChat logic and server message logic

--- server/src/main/java/de/uol/swp/server/chat/ChatService.java
+++ server/src/main/java/de/uol/swp/server/chat/ChatService.java
@@ -3,24 +3,28 @@ package de.uol.swp.server.chat;
 import com.google.inject.Inject;
 import de.uol.swp.common.chat.messages.AbstractChatMessage;
 import de.uol.swp.common.chat.messages.PlayerChatMessage;
+import de.uol.swp.common.chat.messages.ServerChatMessage;
 import de.uol.swp.common.chat.request.GetChatRequest;
 import de.uol.swp.common.chat.request.SendChatRequest;
-import de.uol.swp.common.chat.messages.ServerChatMessage;
+import de.uol.swp.common.chat.response.GetChatResponse;
+import de.uol.swp.common.game.message.response.StatusResponse;
 import de.uol.swp.common.user.Session;
 import de.uol.swp.server.AbstractService;
 import de.uol.swp.server.chat.data.IChatMessage;
+import de.uol.swp.server.chat.event.ServerMessageEvent;
 import de.uol.swp.server.chat.management.IChatManagement;
 import de.uol.swp.server.lobby.data.ILobby;
 import de.uol.swp.server.lobby.management.ILobbyManagement;
 import de.uol.swp.server.lobby.management.LobbyManagement;
 import de.uol.swp.server.usermanagement.IUser;
-import de.uol.swp.server.usermanagement.User;
 import de.uol.swp.server.usermanagement.UserMapper;
 import de.uol.swp.server.usermanagement.exceptions.SessionNotFoundException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
+
+import java.util.List;
 
 public class ChatService extends AbstractService {
     private static final Logger LOG = LogManager.getLogger(ChatService.class);
@@ -61,6 +65,11 @@ public class ChatService extends AbstractService {
         LOG.info("[LobbyId: {}] Chat message sent to all users in lobby", chatMessage.getLobbyId());
     }
 
+    /**
+     * Handles the GetChatRequest event.
+     *
+     * @param request the GetChatRequest containing the details of the request
+     */
     @Subscribe
     public void onGetChatRequest(GetChatRequest request) {
         LOG.debug("[LobbyId: {}] Received request to get chat messages", request.getLobbyId());
@@ -71,10 +80,50 @@ public class ChatService extends AbstractService {
                                  });
         IUser user = UserMapper.toUser(session.getUser());
 
-
-        LOG.info("[LobbyId: {}] Chat messages sent to user", request.getLobbyId());
+        try {
+            List<IChatMessage> chatMessages = chatManagement.getChatMessages(request.getLobbyId(), user);
+            List<AbstractChatMessage> messages = chatMessages.stream()
+                                                             .map(this::convertToChatMessage)
+                                                             .toList();
+            GetChatResponse response = new GetChatResponse(request.getLobbyId(), messages);
+            response.setSession(session);
+            request.getMessageContext()
+                   .ifPresent(response::setMessageContext);
+            LOG.info("[LobbyId: {}] Chat messages sent to user", request.getLobbyId());
+        } catch (IllegalAccessException e) {
+            LOG.error("[LobbyId: {}] User is not allowed to access chat messages", request.getLobbyId());
+            StatusResponse response = new StatusResponse(request.getLobbyId(),
+                    false,
+                    "Nutzer hat keinen Zugriff auf die angefragten Chat-Nachrichten"
+            );
+            response.setSession(session);
+            request.getMessageContext()
+                   .ifPresent(response::setMessageContext);
+            post(response);
+        }
     }
 
+    /**
+     * Handles the ServerMessageEvent event.
+     *
+     * @param event the ServerMessageEvent containing the server message details
+     */
+    @Subscribe
+    public void onServerMessageEvent(ServerMessageEvent event) {
+        LOG.debug("[LobbyId: {}] Received server message", event.getLobbyId());
+        IChatMessage chatMessage = chatManagement.addChatMessage(event.getLobbyId(), event.getMessage());
+
+        AbstractChatMessage message = convertToChatMessage(chatMessage);
+        ILobby lobby = lobbyManagement.getLobby(event.getLobbyId());
+        sendToAllInLobby(lobby, message);
+    }
+
+    /**
+     * Converts an IChatMessage to an AbstractChatMessage.
+     *
+     * @param chatMessage the IChatMessage to convert
+     * @return the converted AbstractChatMessage
+     */
     private AbstractChatMessage convertToChatMessage(IChatMessage chatMessage) {
         if (chatMessage instanceof PlayerChatMessage playerChatMessage) {
             return new PlayerChatMessage(chatMessage.getLobbyId(),
