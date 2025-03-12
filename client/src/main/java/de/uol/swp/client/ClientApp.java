f75c5bf625d7903824fc2d3e8b5cf44cb6eaf955
marten.wempe <marten.wempe@vierol.de>
2025-03-13 00:14:40 +0100

    bugs

--- client/src/main/java/de/uol/swp/client/ClientApp.java
+++ client/src/main/java/de/uol/swp/client/ClientApp.java
@@ -61,7 +61,7 @@ public class ClientApp extends Application implements ConnectionListener {
         List<String> args = p.getRaw();
 
         if (args.size() != 2) {
-            host = "localhost";
+            host = "swp-ibs.de";
             port = Configuration.getDefaultPort();
             LOG.info("Usage: {} host port", ClientConnection.class.getSimpleName());
             LOG.info("Using default port {} {}", port, host);
